600f7e180b4abbfad4a10b8a480ecb1c9aba9854
Mohamed Abd El-Moneim <103948638+mohamedmonem14@users.noreply.github.com>
2024-10-27 21:04:46 +0300

    Update UsersModule.js

--- Le-Chef/modules/UsersModule.js
+++ Le-Chef/modules/UsersModule.js
@@ -7,7 +7,7 @@ const userSchema = new mongoose.Schema({
         unique: true,
         minlength: [3, 'Username is too short'],
         maxlength: [20, 'Username is too long'],
-        trim: true, // To handle name before save from leading and trailing whitespaces
+        trim: true,
     },
     email: {
         type: String,
@@ -15,7 +15,7 @@ const userSchema = new mongoose.Schema({
         unique: true,
         trim: true,
         lowercase: true,
-        match: [/^\S+@\S+\.\S+$/, 'Please enter a valid email address'], // Checks if the email has a valid format
+        match: [/^\S+@\S+\.\S+$/, 'Please enter a valid email address'],
     },
     password: {
         type: String,
@@ -24,17 +24,17 @@ const userSchema = new mongoose.Schema({
     },
     firstName: {
         type: String,
-        required: true, // First name is now required
-        trim: true, // Handles leading and trailing whitespaces
+        required: true,
+        trim: true,
     },
     lastName: {
         type: String,
-        required: true, // Last name is now required
-        trim: true, // Handles leading and trailing whitespaces
+        required: true,
+        trim: true,
     },
     role: {
         type: String,
-        enum: ['user', 'admin'], // Add more roles as needed
+        enum: ['user', 'admin'],
         default: 'user',
     },
     phone: {
@@ -42,49 +42,25 @@ const userSchema = new mongoose.Schema({
         trim: true,
     },
     address: {
-        apartment: {
-            type: String,
-            default: 'NA',
-        },
-        floor: {
-            type: String,
-            default: 'NA',
-        },
-        building: {
-            type: String,
-            default: 'NA',
-        },
-        street: {
-            type: String,
-            default: 'NA',
-        },
-        city: {
-            type: String,
-            default: 'NA',
-        },
-        state: {
-            type: String,
-            default: 'NA',
-        },
-        zip_code: {
-            type: String,
-            default: 'NA',
-        },
-        country: {
-            type: String,
-            default: 'NA',
-        },
+        apartment: { type: String, default: 'NA' },
+        floor: { type: String, default: 'NA' },
+        building: { type: String, default: 'NA' },
+        street: { type: String, default: 'NA' },
+        city: { type: String, default: 'NA' },
+        state: { type: String, default: 'NA' },
+        zip_code: { type: String, default: 'NA' },
+        country: { type: String, default: 'NA' },
     },
     token: {
         type: String,
     },
     image: {
-        public_id: {
-            type: String,
-        },
-        url: {
-            type: String,
-        },
+        public_id: { type: String },
+        url: { type: String },
+    },
+    educationLevel: {
+        type: Number,
+        enum: [1, 2, 3], // Levels like 1, 2, 3, 4, etc.
     },
     created_at: {
         type: Date,
