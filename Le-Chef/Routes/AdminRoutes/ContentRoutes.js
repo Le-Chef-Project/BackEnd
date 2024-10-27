8174bce216982db290eaf073914f4dcf7f9c1a9b
Mohamed Abd El-Moneim <103948638+mohamedmonem14@users.noreply.github.com>
2024-10-27 21:04:55 +0300

    Update ContentRoutes.js

--- Le-Chef/Routes/AdminRoutes/ContentRoutes.js
+++ Le-Chef/Routes/AdminRoutes/ContentRoutes.js
@@ -5,6 +5,9 @@ const { AddNote } = require('../../Controllers/Admin/ContentManagement/NotesCont
 const { getAllNotes } = require('../../Controllers/Admin/ContentManagement/NotesController');
 const { updateNote } = require('../../Controllers/Admin/ContentManagement/NotesController'); 
 const { deleteNote } = require('../../Controllers/Admin/ContentManagement/NotesController'); 
+const { getAllNotesForUserLevel } = require('../../Controllers/Admin/ContentManagement/NotesController');
+const { userMiddleware } = require('../../Middleware/User');
+
 
 
 
@@ -14,6 +17,7 @@ const { deletePDF } = require('../../Controllers/Admin/ContentManagement/PdfCont
 const { adminMiddleware } = require('../../Middleware/Admin');
 
 
+
 const {UploadVideo, getAllVideos, getVideoById, updateVideo, deleteVideo } = require('../../Controllers/Admin/ContentManagement/VideoConroller'); 
 
 
@@ -21,6 +25,7 @@ router.route('/Notes').post(adminMiddleware,AddNote);
 router.route('/ShowAllNotes').get(getAllNotes);
 router.route('/UpdateNotes/:id').put(adminMiddleware,updateNote);
 router.route('/DeleteNotes/:id').delete(adminMiddleware,deleteNote);
+router.route('/ShowLevelNotes').get(userMiddleware,getAllNotesForUserLevel);
 
 
 
