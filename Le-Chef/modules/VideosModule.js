4212c4773f315d737376bab7e36f498e80ff4e0f
Mohamed Abd El-Moneim <103948638+mohamedmonem14@users.noreply.github.com>
2024-10-27 21:04:50 +0300

    Update VideosModule.js

--- Le-Chef/modules/VideosModule.js
+++ Le-Chef/modules/VideosModule.js
@@ -1,15 +1,44 @@
 const mongoose = require('mongoose');
 
 const videoSchema = new mongoose.Schema({
-  title: String,
-  description: String,
+  title: {
+    type: String,
+    required: true, // Title is required
+  },
+  description: {
+    type: String,
+    required: true, // Description is required
+  },
   isLocked: {
     type: Boolean,
     default: true, // Default to locked
   },
-  url: String, // URL of the stored video
-  teacher: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
-  createdAt: { type: Date, default: Date.now }
+  url: {
+    type: String,
+    required: true, // URL is required
+  },
+  teacher: { 
+    type: mongoose.Schema.Types.ObjectId, 
+    ref: 'User',
+    required: true, // Teacher reference is required
+  },
+  educationLevel: {
+    type: Number,
+    enum: [1, 2, 3], // Define levels as required
+    required: true,
+  },
+  amountToPay: {
+    type: Number,
+    required: function() { return this.paid; }, // Required if video is paid
+  },
+  paid: {
+    type: Boolean,
+    default: true, // Default to false (indicating the video is free)
+  },
+  createdAt: { 
+    type: Date, 
+    default: Date.now 
+  }
 });
 
 const Video = mongoose.model('Video', videoSchema);
