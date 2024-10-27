7481c25ef21ed3cf959d3c7a35cd1b8d4e38d1e8
Mohamed Abd El-Moneim <103948638+mohamedmonem14@users.noreply.github.com>
2024-10-27 21:02:22 +0300

    Add level

--- Le-Chef/Controllers/Admin/ContentManagement/NotesController.js
+++ Le-Chef/Controllers/Admin/ContentManagement/NotesController.js
@@ -1,4 +1,6 @@
 const noteModel = require('../../../modules/NotesModule');
+const userModule = require('../../../modules/UsersModule');
+
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const jwt = require('jsonwebtoken'); // Assuming you're using JWT for tokens
@@ -7,12 +9,13 @@ const jwt = require('jsonwebtoken'); // Assuming you're using JWT for tokens
 exports.AddNote = async (req, res) => {
     try {
 
-      const {content} = req.body;
+      const {content,educationLevel} = req.body;
       const token = req.headers.token
       const decoded = jwt.verify(token, 'your_secret_key'); // Replace with your actual secret key
       const teacherId = decoded._id; // Assuming '_id' contains the teacher's ID
       const note = new noteModel({
         content,
+        educationLevel,
         teacher: teacherId })// Use the teacher's existing ID from the token});
       await note.save();
       res.status(201).json(note);
@@ -35,11 +38,12 @@ exports.AddNote = async (req, res) => {
 
 exports.updateNote = async (req, res) => {
     try {
-        const { content } = req.body;
+        const { content ,educationLevel} = req.body;
         const updatedNote = await noteModel.findByIdAndUpdate(
             req.params.id,
             {
                 content,
+                educationLevel,
                 updatedAt: Date.now()
             },
             { new: true } // Return the updated document
@@ -63,6 +67,25 @@ exports.deleteNote = async (req, res) => {
             return res.status(404).json({ message: 'Note not found' });
         }
         res.status(200).json({ message: 'Note deleted successfully' });
+    } catch (error) {
+        res.status(400).json({ error: error.message });
+    }
+};
+
+
+exports.getAllNotesForUserLevel = async (req, res) => {
+    try {
+        // Fetch the user based on the ID stored in `req.user`
+        const user = await userModule.findById(req.user._id);
+        if (!user) {
+            return res.status(404).json({ error: 'User not found' });
+        }
+
+        // Find notes that match the user's education level
+        const notes = await noteModel.find({ educationLevel: user.educationLevel })
+           
+
+        res.status(200).json(notes);
     } catch (error) {
         res.status(400).json({ error: error.message });
     }
