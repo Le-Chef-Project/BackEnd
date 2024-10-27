a23079c80b89b88e10bf06d833c81041281c1a01
Mohamed Abd El-Moneim <103948638+mohamedmonem14@users.noreply.github.com>
2024-10-27 21:02:49 +0300

    Add level

--- Le-Chef/modules/NotesModule.js
+++ Le-Chef/modules/NotesModule.js
@@ -2,12 +2,26 @@ const mongoose = require('mongoose');
 const timezonePlugin = require('../timezonePlugin');
 
 const noteSchema = new mongoose.Schema({
-  content: String,
-  teacher: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
-  createdAt: { type: Date, default: Date.now }
-}, { id: false }); // Disable the automatic `id` virtual property
+  content: {
+    type: String,
+    required: true,
+  },
+  teacher: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true,
+  },
+  educationLevel: {
+    type: Number,
+    enum: [1, 2, 3], // Define levels as required
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+}, { id: false });
 
-// Use the plugin with the timezone
 noteSchema.plugin(timezonePlugin, { timezone: 'Africa/Cairo' });
 
 const Note = mongoose.model('Note', noteSchema);
