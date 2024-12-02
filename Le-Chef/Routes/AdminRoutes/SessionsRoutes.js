935979311902cfbc41603b25b5a63bdf6516143c
thomasarmanious <armaniousthomas@gmail.com>
2024-12-02 19:23:25 +0200

    delete old sessions

--- Le-Chef/Routes/AdminRoutes/SessionsRoutes.js
+++ Le-Chef/Routes/AdminRoutes/SessionsRoutes.js
@@ -3,19 +3,8 @@ const router=express.Router();
 const { adminMiddleware } = require('../../Middleware/Admin');
 const { userMiddleware } = require('../../Middleware/User');
 
-const { generateRtcToken } = require('../../Controllers/Admin/SessionsManagement/SessionsController'); 
-const { createSession } = require('../../Controllers/Admin/SessionsManagement/SessionsController'); 
-const { getSessions } = require('../../Controllers/Admin/SessionsManagement/SessionsController'); 
-
-
-
 const { createZoomMeeting } = require('../../Controllers/Admin/SessionsManagement/zoomService'); 
 const { joinZoomMeeting } = require('../../Controllers/Admin/SessionsManagement/zoomService'); 
-
-
-router.post('/RTC', generateRtcToken);
-router.post('/CreateSession', createSession);
-router.get('/GetSession', getSessions);
 
 
 
